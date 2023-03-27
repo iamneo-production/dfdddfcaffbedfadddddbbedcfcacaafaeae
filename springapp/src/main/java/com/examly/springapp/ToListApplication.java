cc7c6a2b4e0af6e01cabec0c2d67780468b87517
root <root@dfdddfcaffbedfadddddbbedcfcacaafaeae-0.dfdddfcaffbedfadddddbbedcfcacaafaeae.che.svc.cluster.local>
2023-03-27 05:26:06 +0000

    Backup Branch before deleting

---
+++ springapp/src/main/java/com/examly/springapp/ToListApplication.java
@@ -0,0 +1,13 @@
+package com.examly.springapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToListApplication.class, args);
+	}
+
+}
